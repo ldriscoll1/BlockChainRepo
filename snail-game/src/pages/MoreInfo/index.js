08d9d813548c4ea1e74e79725d3d69007fdb6364
ryrysmiley <>
2023-05-08 23:35:58 -0700

    more info

--- snail-game/src/pages/MoreInfo/index.js
+++ snail-game/src/pages/MoreInfo/index.js
@@ -1,7 +1,14 @@
+import styles from '../../styles/Home.module.css';
 export default function MoreInfo() {
 	return (
-		<div>
-			<h1>More Info</h1>
+		<div className={styles.MoreInfo}>
+			<h1>Resources</h1>
+			<ul>
+				<li><a href="https://docs.soliditylang.org/en/v0.8.6/">Solidity Documentation</a></li>
+				<li><a href="https://info.etherscan.com/how-to-use-read-or-write-contract-features-on-etherscan/">How to Use Read or Write Contract Features on Etherscan</a></li>
+				<li><a href="https://nextjs.org/docs">Nextjs Documentation</a></li>
+				<li><a href="https://docs.etherscan.io/tutorials/read-write-smart-contracts">Read/Write Smart Contracts through Etherscan API</a></li>
+			</ul>
 		</div>
 	);
 }
